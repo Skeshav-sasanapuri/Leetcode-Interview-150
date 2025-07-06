190fe673b6307314a06841351e37b584f7921545
Skeshav-sasanapuri <150312135+Skeshav-sasanapuri@users.noreply.github.com>
2025-07-06 18:26:50 -0400

    Time: 19 ms (67.77%) | Memory: 18.6 MB (41.91%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -5,8 +5,9 @@ class Solution:
         for i in range(l):
             if i > max_reachable:
                 return False
-            if max_reachable < i + nums[i]:
-                max_reachable = i + nums[i]
+            max_r = i+ nums[i]
+            if max_reachable < max_r:
+                max_reachable = max_r
             # max_reachable = max(i + nums[i], max_reachable)
         
         return True
