ca89cbedbd1c4dec926e1c7307817e3db075f3b2
Skeshav-sasanapuri <150312135+Skeshav-sasanapuri@users.noreply.github.com>
2025-07-11 09:49:09 -0400

    Time: 144 ms (44.95%) | Memory: 58 MB (40.16%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -3,25 +3,35 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.s = []
+        self.values_list = []
+        self.index_dict = {}
+        self.curr_last_index =  0
 
     def insert(self, val: int) -> bool:
-        for element in self.s:
-            if element == val:
-                return False
-        self.s.append(val)
-        return True
+        if val not in self.index_dict:
+            self.values_list.append(val)
+            self.index_dict[val] = self.curr_last_index
+            self.curr_last_index += 1
+            return True
+        else:
+            return False
 
     def remove(self, val: int) -> bool:
-        for i in range(len(self.s)):
-            if self.s[i] == val:
-                self.s.pop(i)
-                return True
-        return False
+        if val in self.index_dict:
+            update_dict_key = self.values_list[self.curr_last_index-1]
+            update_dict_val = self.index_dict[val]
+            self.values_list[self.curr_last_index-1], self.values_list[self.index_dict[val]] = self.values_list[self.index_dict[val]], self.values_list[self.curr_last_index-1] 
+            self.index_dict[update_dict_key] = update_dict_val
+            self.values_list.pop()
+            self.index_dict.pop(val)
+            self.curr_last_index -= 1
+            return True
+        else:
+            return False
 
     def getRandom(self) -> int:
-        index = random.randint(0, len(self.s)-1)
-        return self.s[index]
+        rand_index = random.randint(0, self.curr_last_index-1)
+        return self.values_list[rand_index]        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
